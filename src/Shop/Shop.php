10587ff3fdc7dd194fc6080dee4afc9f3acfa662
Rein Van Oyen <reinvanoyen@gmail.com>
2019-11-26 15:24:10 +0100

    progress

--- src/Shop/Shop.php
+++ src/Shop/Shop.php
@@ -2,64 +2,15 @@
 
 namespace Tnt\Ecommerce\Shop;
 
-use Tnt\Ecommerce\Contracts\DiscountInterface;
 use Tnt\Ecommerce\Contracts\FulfillmentInterface;
-use Tnt\Ecommerce\Contracts\PaymentInterface;
 use Tnt\Ecommerce\Contracts\ShopInterface;
 
 class Shop implements ShopInterface
 {
-    // @TODO clean this shit up
-
-    /**
-     * @var array $payments
-     */
-    private $payments = [];
-
     /**
      * @var array $fulfillments
      */
     private $fulfillments = [];
-
-    /**
-     * @var array $discounts
-     */
-    private $discounts = [];
-
-    /**
-     * @param PaymentInterface $payment
-     * @return mixed|void
-     */
-    public function addPayment(PaymentInterface $payment)
-    {
-        $this->payments[$payment->getId()] = $payment;
-    }
-
-    /**
-     * @param int $id
-     * @return PaymentInterface
-     */
-    public function getPayment($id): PaymentInterface
-    {
-        return $this->payments[$id];
-    }
-
-    /**
-     * @param $id
-     * @return bool
-     */
-    public function hasPayment($id): bool
-    {
-        return isset($this->payments[$id]);
-    }
-
-    /**
-     * @return array
-     */
-    public function getPayments(): array
-    {
-        return $this->payments;
-    }
 
     /**
      * @param FulfillmentInterface $fulfillment
@@ -94,40 +45,5 @@ class Shop implements ShopInterface
     public function getFulfillments(): array
     {
         return $this->fulfillments;
-    }
-
-    /**
-     * @param DiscountInterface $discount
-     * @return mixed|void
-     */
-    public function addDiscount(DiscountInterface $discount)
-    {
-        $this->discounts[$discount->getId()] = $discount;
-    }
-
-    /**
-     * @param $id
-     * @return DiscountInterface
-     */
-    public function getDiscount($id): DiscountInterface
-    {
-        return $this->discounts[$id];
-    }
-
-    /**
-     * @param DiscountInterface $discount
-     * @return bool
-     */
-    public function hasDiscount(DiscountInterface $discount): bool
-    {
-        return isset($this->discounts[$discount->getId()]);
-    }
-
-    /**
-     * @return array
-     */
-    public function getDiscounts(): array
-    {
-        return $this->discounts;
     }
 }
