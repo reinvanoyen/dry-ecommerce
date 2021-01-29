eb90d2ac074b811adbe62006ff77fb8a6cf81b84
dieter <dietervyncke@Dieters-iMac-2.local>
2021-01-29 10:46:51 +0100

    Updated PaymentRefunded, added refunds

--- src/Events/Order/PaymentRefunded.php
+++ src/Events/Order/PaymentRefunded.php
@@ -2,7 +2,28 @@
 
 namespace Tnt\Ecommerce\Events\Order;
 
+use Tnt\Ecommerce\Contracts\OrderInterface;
+
 class PaymentRefunded extends OrderEvent
 {
-    //
+    private $refunds;
+
+    /**
+     * PaymentRefunded constructor.
+     * @param OrderInterface $order
+     * @param $refunds
+     */
+    public function __construct(OrderInterface $order, $refunds)
+    {
+        $this->refunds = $refunds;
+        parent::__construct($order);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefunds()
+    {
+        return $this->refunds;
+    }
 }
